ac6582d75d931179e2ce8709f69f1b0fe8217548
Gokulakrishna <gokulakrishnaks@gmail.com>
2018-05-21 16:09:08 +0530

    Corrected typo ; to replace ,

--- js/redirect.js
+++ js/redirect.js
@@ -245,7 +245,7 @@ Redirect.prototype = {
   },
 
   _init: function(o) {
-    this.description = o.description || '',
+    this.description = o.description || '';
       this.exampleUrl = o.exampleUrl || '';
     this.exampleResult = o.exampleResult || '';
     this.error = o.error || null;
