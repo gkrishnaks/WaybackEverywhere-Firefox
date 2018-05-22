76430903e0a97769cfd01b613a10c1e3eae8aed5
Gokulakrishna <gokulakrishnaks@gmail.com>
2018-05-22 15:36:34 +0530

    Update background.js

--- js/background.js
+++ js/background.js
@@ -363,7 +363,7 @@ function checkRedirects(details) {
     // Issue 12   https://github.com/gkrishnaks/WaybackEverywhere-Firefox/issues/12
     let isJustSaved=false;  
     for(let k=0; k < justSaved.length; k++){
-        if(urlDetails.url == justSaved[k].split("==WBE==").shift()){
+        if(urlDetails.url == justSaved[k].split("==WBE==")[0]){
             isJustSaved=true;
             break;
         }
